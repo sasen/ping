ed28ad390383f15bc1f365d80ae318e0a404d67b
Sasen Cain <sasen@mit.edu>
2015-03-08 20:38:48 -0700

    ss: phenx subsetting

--- ses_beh_model.R
+++ ses_beh_model.R
@@ -4,6 +4,14 @@ library(magrittr)
 
 BehaveData <- read.csv("Data/PING_Behavior.csv",stringsAsFactors=FALSE)
 
+phx = subset(BehaveData, Age_At_PhenX_Completion>1)
+no_phx = subset(BehaveData, is.na(Age_At_PhenX_Completion))
+range(no_phx$Age,na.rm=TRUE)  # 3 21
+range(phx$Age,na.rm=TRUE)  # 7.08 21.00
+range(phx$Age_At_PhenX_Completion,na.rm=TRUE)  # 8.25 22.67
+
+
+
 ## SES measures
 inc = BehaveData$FDH_3_Household_Income
 
